085972ff9ceb8d4d00638e1540adf99ba0760544
wizardforcel <562826179@qq.com>
2022-08-15 18:05:49 +0800

    2022-08-15 18:05:49

---
+++ BookerWikiTool/util.py
@@ -0,0 +1,16 @@
+
+headers = {
+    'User-Agent': 'PostmanRuntime/7.26.8',
+    'Referer': 'https://www.bilibili.com/',
+}
+
+def fname_escape(name):
+    return name.replace('\\', '＼') \
+               .replace('/', '／') \
+               .replace(':', '：') \
+               .replace('*', '＊') \
+               .replace('?', '？') \
+               .replace('"', '＂') \
+               .replace('<', '＜') \
+               .replace('>', '＞') \
+               .replace('|', '｜')
