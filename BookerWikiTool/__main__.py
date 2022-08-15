567917b170954e7990d591e69c56f31d6e801ffc
wizardforcel <562826179@qq.com>
2022-08-15 18:21:03 +0800

    2022-08-15 18:21:03

--- BookerWikiTool/__main__.py
+++ BookerWikiTool/__main__.py
@@ -20,49 +20,7 @@ from EpubCrawler.img import process_img
 from EpubCrawler.util import safe_mkdir
 from . import __version__
 from .bili import *
-
-DIR = path.dirname(path.abspath(__file__))
-
-default_hdrs = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36',
-}
-
-RE_YAML_META = r'<!--yml([\s\S]+?)-->'
-RE_TITLE = r'^#+ (.+?)$'
-RE_CODE_BLOCK = r'```[\s\S]+?```'
-RE_IMG = r'!\[.*?\]\(.*?\)'
-# Word 字数统计标准：
-# 一个汉字或中文标点算一个字
-# 一个连续的英文字母、标点和数字序列算一个字
-RE_ZH_WORD = r'[\u2018-\u201d\u3001-\u301c\u4e00-\u9fff\uff01-\uff65]'
-RE_EN_WORD = r'[\x21-\x7e]+'
-
-def d(name):
-    return path.join(DIR, name)
-
-def tomd(html):
-    js_fname = d('tomd.js')
-    html_fname = path.join(tempfile.gettempdir(), uuid.uuid4().hex + '.html')
-    open(html_fname, 'w', encoding='utf8').write(html)
-    subp.Popen(
-        ["node", js_fname, html_fname],
-        shell=True,
-    ).communicate()
-    md_fname = re.sub(r'\.html$', '', html_fname) + '.md'
-    md = open(md_fname, encoding='utf8').read()
-    os.remove(html_fname)
-    return md
-
-def fname_escape(name):
-    return name.replace('\\', '＼') \
-               .replace('/', '／') \
-               .replace(':', '：') \
-               .replace('*', '＊') \
-               .replace('?', '？') \
-               .replace('"', '＂') \
-               .replace('<', '＜') \
-               .replace('>', '＞') \
-               .replace('|', '｜')
+from .util import *
 
 def account_handle(args):
     if not args.file.endswith('.md'):
@@ -70,12 +28,7 @@ def account_handle(args):
         return
     print(args.file)
     cont = open(args.file, encoding='utf8').read()
-    # 去掉代码块和图片
-    cont = re.sub(RE_CODE_BLOCK, '', cont)
-    cont = re.sub(RE_IMG, '', cont)
-    zh_count = len(re.findall(RE_ZH_WORD, cont))
-    en_count = len(re.findall(RE_EN_WORD, cont))
-    total = zh_count + en_count
+    total, zh_count, en_count = account_words(cont)
     print(f'中文字数：{zh_count}\n英文字数：{en_count}\n总字数：{total}')
 
 def fix_handle(args):
--- BookerWikiTool/util.py
+++ BookerWikiTool/util.py
@@ -1,8 +1,53 @@
+import tempfile
+import uuid
+import subprocess as subp
+import re
+import os
+import json
+import yaml
+from urllib.parse import quote_plus
+from os import path
+from pyquery import PyQuery as pq
+from datetime import datetime
+from collections import OrderedDict
+
+RE_YAML_META = r'<!--yml([\s\S]+?)-->'
+RE_TITLE = r'^#+ (.+?)$'
+RE_CODE_BLOCK = r'```[\s\S]+?```'
+RE_IMG = r'!\[.*?\]\(.*?\)'
+# Word 字数统计标准：
+# 一个汉字或中文标点算一个字
+# 一个连续的英文字母、标点和数字序列算一个字
+RE_ZH_WORD = r'[\u2018-\u201d\u3001-\u301c\u4e00-\u9fff\uff01-\uff65]'
+RE_EN_WORD = r'[\x21-\x7e]+'
+
+DIR = path.dirname(path.abspath(__file__))
+
+default_hdrs = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36',
+}
 
 headers = {
     'User-Agent': 'PostmanRuntime/7.26.8',
     'Referer': 'https://www.bilibili.com/',
 }
+
+
+def d(name):
+    return path.join(DIR, name)
+
+def tomd(html):
+    js_fname = d('tomd.js')
+    html_fname = path.join(tempfile.gettempdir(), uuid.uuid4().hex + '.html')
+    open(html_fname, 'w', encoding='utf8').write(html)
+    subp.Popen(
+        ["node", js_fname, html_fname],
+        shell=True,
+    ).communicate()
+    md_fname = re.sub(r'\.html$', '', html_fname) + '.md'
+    md = open(md_fname, encoding='utf8').read()
+    os.remove(html_fname)
+    return md
 
 def fname_escape(name):
     return name.replace('\\', '＼') \
@@ -14,3 +59,12 @@ def fname_escape(name):
                .replace('<', '＜') \
                .replace('>', '＞') \
                .replace('|', '｜')
+               
+def account_words(cont):
+    # 去掉代码块和图片
+    cont = re.sub(RE_CODE_BLOCK, '', cont)
+    cont = re.sub(RE_IMG, '', cont)
+    zh_count = len(re.findall(RE_ZH_WORD, cont))
+    en_count = len(re.findall(RE_EN_WORD, cont))
+    total = zh_count + en_count
+    return (total, zh_count, en_count)
