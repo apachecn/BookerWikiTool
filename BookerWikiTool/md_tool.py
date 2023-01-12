aa166c4626b6b6bd05d7167d228103f4c21059b5
wizardforcel <562826179@qq.com>
2023-01-12 19:50:10 +0800

    2023-01-12 19:50:10

--- BookerWikiTool/md_tool.py
+++ BookerWikiTool/md_tool.py
@@ -276,3 +276,45 @@ def opti_md_dir(args):
         pool.apply_async(opti_md_file_safe, [args])
     pool.close()
     pool.join()
+
+
+def config_proj(args):
+    dir = path.abspath(dir)
+    if not path.isdir(dir):
+        print('请提供目录')
+        return
+    fnames = os.listdir(dir)
+    if 'README.md' not in fnames or \
+        'index.html' not in fnames or \
+        'CNAME' not in fnames:
+        print('缺少 README.md，index.html 或 CNAME 文件')
+        return
+    name = input('请输入中文名称：').strip() or '{name}'
+    name_en = input('请输入英文名称：').strip() or '{nameEn}'
+    url_en = input('请输入英文链接：').strip() or '{urlEn}'
+    domain = input('请输入域名前缀：').strip() or '{domain}'
+    color = input('请输入颜色：').strip() or '{color}'
+    repo = path.basename(dir)
+    
+    conts = {
+        'README.md': open(path.join(dir, 'README.md'), encoding='utf8').read(),
+        'index.html': open(path.join(dir, 'index.html'), encoding='utf8').read(),
+        'CNAME': open(path.join(dir, 'CNAME'), encoding='utf8').read(),
+    }
+    for fname, cont in conts.items():
+        cont = (
+            cont.replace('{name}', name)
+                .replace('{nameEn}', name_en)
+                .replace('{urlEn}', url_en)
+                .replace('{urlEn}', url_en)
+                .replace('{domain}', domain)
+                .replace('{color}', color)
+                .replace('{adminName}', '飞龙')
+                .replace('{adminUn}', 'wizardforcel')
+                .replace('{adminQq}', '562826179')
+                .replace('{repo}', repo)
+                .replace('{dockerName}', repo)
+                .replace('{pypiName}', repo)
+                .replace('{npmName}', repo)
+        )
+        open(path.join(dir, fname), 'w', encoding='utf8').write(cont)
