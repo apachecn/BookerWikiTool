ce0168d92c0d07ce1e9c37ab362d1434abc62c99
wizardforcel <562826179@qq.com>
2023-01-20 16:20:46 +0800

    2023-01-20 16:20:46

--- BookerWikiTool/pdf_tool.py
+++ BookerWikiTool/pdf_tool.py
@@ -14,6 +14,15 @@ from .util import *
 
 ImageFile.LOAD_TRUNCATED_IMAGES = True
 
+app_map = {
+    'ppt': ['PowerPoint.Application', 'Presentations'],
+    'pptx': ['PowerPoint.Application', 'Presentations'],
+    'doc': ['Word.Application', 'Documents'],
+    'docx': ['Word.Application', 'Documents'],
+    'xls': ['Excel.Application', 'Workbooks'],
+    'xlsx': ['Excel.Application', 'Workbooks'],
+}
+
 def ext_pdf(args):
     if path.isdir(args.fname):
         ext_pdf_dir(args)
@@ -114,33 +123,36 @@ def pack_pdf(args):
         print(fname)
         open(fname, 'wb').write(pdf)
 
-def ppt2pdf(fname, ofname):
-    app = win32com.client.Dispatch('PowerPoint.Application')
-    ppt = app.Presentations.Open(fname)
+def office2pdf(fname, ofname):
+    m = re.search(r'\.(\w+)$', fname)
+    ext = m.group(1) if m else ""
+    if ext not in app_map:
+        raise FileError(f'{fname} 不是 DOC、XLS 或 PPT 文件')
+    app = win32com.client.Dispatch(app_map[ext][0])
+    ppt = getattr(app, app_map[ext][1]).Open(fname)
     ppt.SaveAs(ofname, 32)
     app.Quit()
     
-def ppt2pdf_file(args):
+def office2pdf_file(args):
     fname = args.fname
     print(fname)
-    if not fname.endswith('.ppt') and \
-        not fname.endswith('.pptx'):
-            print('请提供 PPT 文件')
-            return
+    m = re.search(r'\.(\w+)$', fname)
+    ext = m.group(1) if m else ""
+    if ext not in app_map:
+        print('请提供 DOC、XLS 或 PPT 文件')
+        return
     fname = path.join(os.getcwd(), fname)
-    ofname = fname.replace('.ppt', '') \
-        .replace('.pptx', '') + '.pdf'
-    ofname = path.join(os.getcwd(), ofname)
-    ppt2pdf(fname, ofname)
+    ofname = re.sub(r'\.\w+$', '', fname) + '.pdf'
+    office2pdf(fname, ofname)
     print("转换成功！")
 
-def ppt2pdf_dir(args):
+def office2pdf_dir(args):
     dir = args.fname
     fnames = os.listdir(dir)
     for f in fnames:
         ff = path.join(dir, f)
         args.fname = ff
-        try: ppt2pdf_file(args)
+        try: office2pdf_file(args)
         except Exception as ex: traceback.print_exc()
 
 def waifu2x_auto_file_safe(args):
